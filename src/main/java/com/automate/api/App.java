20ca3e977efb0e2bb779336aac98c57f08e8082c
Ishan Kumar <ishan.dns@gmail.com>
2019-07-22 17:58:38 +0530

    Add files via upload

---
+++ src/main/java/com/automate/api/App.java
@@ -0,0 +1,13 @@
+package com.automate.api;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
